a460a0b6ce199770398abceb6f05641ff7c4fa09
every3743 <every3743@gmail.com>
2022-03-09 08:43:26 +0900

    0 입력될 때까지 무한 출력하기1

---
+++ src/codeUp/_1071.java
@@ -0,0 +1,22 @@
+package codeUp;
+
+import java.util.*;
+
+public class _1071 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int n = sc.nextInt();
+
+			if (n == 0) {
+				break;
+			}
+			System.out.println(n);
+		}
+		sc.close();
+	}
+
+}
