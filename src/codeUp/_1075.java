1808aa5a466ca4bdac93fadc5bd4670ec255ce69
every3743 <every3743@gmail.com>
2022-03-09 09:01:43 +0900

    정수 1개 입력받아 카운트다운 출력하기2

---
+++ src/codeUp/_1075.java
@@ -0,0 +1,17 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class _1075 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = n - 1; i >= 0; i--) {
+			System.out.println(i);
+		}
+		sc.close();
+	}
+}
