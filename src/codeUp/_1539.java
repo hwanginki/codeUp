6681f6b96713356eebace7a98c3b3946554ffe05
every3743 <every3743@gmail.com>
2022-03-10 10:57:31 +0900

    [기초-함수작성] 함수로 false 또는 true 출력하기

---
+++ src/codeUp/_1539.java
@@ -0,0 +1,25 @@
+package codeUp;
+
+import java.util.*;
+
+public class _1539 {
+	
+	static void f(int n) {
+		if (0 == n) {
+			 System.out.print("false");
+		} else {
+			System.out.print("true");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		f(n);
+		sc.close();
+		
+	}
+	
+}
