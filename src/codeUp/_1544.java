7180a22851ae6addfe6ab96f99df0a6fca69713a
every3743 <every3743@gmail.com>
2022-03-10 11:26:36 +0900

    [기초-함수작성] 함수로 * n개 출력하기

---
+++ src/codeUp/_1544.java
@@ -0,0 +1,21 @@
+package codeUp;
+
+import java.util.*;
+
+public class _1544 {
+	
+	static void f(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.print("*");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		f(sc.nextInt());
+		sc.close();
+		
+	}
+	
+}
