d3063768f262eb1fed44076f602be30cba5b4ae8
every3743 <every3743@gmail.com>
2022-03-10 12:13:42 +0900

    [기초-함수작성] 함수의 양의 제곱근의 정수 부분만 리턴하기

---
+++ src/codeUp/_1550.java
@@ -0,0 +1,19 @@
+package codeUp;
+
+import java.util.*;
+
+public class _1550 {
+	
+	static void f(Double i) {
+		System.out.print((int) Math.sqrt(i));
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		f(sc.nextDouble());
+		sc.close();
+		
+	}
+	
+}
