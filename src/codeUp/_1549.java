9405c3df46fe30a4879451a167dace484a769213
every3743 <every3743@gmail.com>
2022-03-10 12:00:43 +0900

    [기초-함수작성] 함수로 절댓값 리턴하기

---
+++ src/codeUp/_1549.java
@@ -0,0 +1,19 @@
+package codeUp;
+
+import java.util.*;
+
+public class _1549 {
+	
+	static void f(long i) {
+		System.out.println(Math.abs(i));
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		f(sc.nextLong());
+		sc.close();
+		
+	}
+	
+}
