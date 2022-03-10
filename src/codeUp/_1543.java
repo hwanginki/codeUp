ebdb62fc59a88d26c692ce8a0298a6f61a07c99a
every3743 <every3743@gmail.com>
2022-03-10 11:25:16 +0900

    [기초-함수작성] 함수로 love 출력하기

---
+++ src/codeUp/_1543.java
@@ -0,0 +1,23 @@
+package codeUp;
+
+import java.util.*;
+
+public class _1543 {
+	
+	static void f(int n) {
+		int i = 0;
+		while (i < n) {
+			System.out.println("love");
+			i++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		f(sc.nextInt());
+		sc.close();
+		
+	}
+	
+}
