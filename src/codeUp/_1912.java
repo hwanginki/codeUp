d982661faf5a6a04485dc4af14e5c221473dfb05
every3743 <every3743@gmail.com>
2022-03-11 18:26:58 +0900

    (재귀함수) 팩토리얼 계산

---
+++ src/codeUp/_1912.java
@@ -0,0 +1,20 @@
+package codeUp;
+
+import java.io.*;
+
+public class _1912 {
+	static int recursive(int x) {
+		if (x == 1) {
+			return 1;
+		} else {
+			return x * recursive(x - 1);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(br.readLine());
+		System.out.println(recursive(x));
+		br.close();
+	}
+}
