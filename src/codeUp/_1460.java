07a9b8a303333a2a8efb458ab1da78ce6cb91b91
every3743 <every3743@gmail.com>
2022-03-13 13:09:52 +0900

    [기초-배열연습] 2차원 배열 순서대로 채우기 1-1

---
+++ src/codeUp/_1460.java
@@ -0,0 +1,26 @@
+package codeUp;
+
+import java.io.*;
+
+public class _1460 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][n];
+		int cnt = 1;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = cnt++;
+				bw.write(String.valueOf(arr[i][j] + " "));
+			}
+			bw.write("\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
