fba44a921543f98fc5e2176d092330265f8e9328
every3743 <every3743@gmail.com>
2022-03-14 12:35:13 +0900

    [기초-배열연습] 2차원 배열 지그재그 채우기 2-2

---
+++ src/codeUp/_1469.java
@@ -0,0 +1,39 @@
+package codeUp;
+
+import java.io.*;
+
+public class _1469 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int x = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[x][x];
+		int cnt = 1;
+
+		for (int i = 0; i < x; i++) {
+			if (i % 2 == 0) {
+				for (int j = x - 1; j >= 0; j--) {
+					arr[i][j] = cnt++;
+				}
+			} else {
+				for (int j = 0; j < x; j++) {
+					arr[i][j] = cnt++;
+				}
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				bw.write(arr[i][j] + " ");
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
